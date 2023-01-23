3efc5dccf6e1aabac950a2c2fda279116c8d7ff4
sol <sol@spoonradio.co>
2023-01-23 21:56:51 +0900

    🐛 Bug: checkTimeValidation 오류 수정

--- src/utils/checkTimeValidation.ts
+++ src/utils/checkTimeValidation.ts
@@ -3,7 +3,9 @@ export const checkTimeValidation = (date1: string, date2: string) => {
   const [hour2, min2]: number[] = date2.split(':').map((s) => Number(s));
 
   if (hour2 - hour1 < 0) return false;
-  if (min2 - min1 < 0) return false;
+  if (hour2 - hour1 > 0) return true;
   if (hour2 - hour1 === 0 && min2 - min1 === 0) return false;
+  if (min2 - min1 < 0) return false;
+
   return true;
 };
